53843c3bfbcfe18a1f8b2686b83359bb4103574a
aleksem07 <aleksem07@gmail.com>
2023-03-09 14:55:27 +0300

    Updates

---
+++ js/main.js
@@ -0,0 +1,2 @@
+
+import'./nav-toggle.js';import'./price-buttons.js';import'./service-filter.js';import'./contact-button.js';
---
+++ js/nav-toggle.js
@@ -0,0 +1,2 @@
+
+const headerWrapper=document.querySelector('.header');const headerButton=headerWrapper.querySelector('.button-nav');const headerMenu=headerWrapper.querySelector('.header__menu');headerMenu.classList.add('close__nav');headerButton.addEventListener('click',(evt)=>{evt.preventDefault();headerMenu.classList.toggle('close__nav');headerButton.classList.toggle('button-nav--closed');headerButton.classList.toggle('button-nav--opened');});
